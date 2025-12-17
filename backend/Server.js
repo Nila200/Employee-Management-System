00d339d8d44f75bbda27b2998c3da1e36bf43acc
Nila <nilavelayutham2001@gmail.com>
2025-12-17 21:23:53 +0530

    Fix .gitignore for backend and frontend folder names and server setup

---
+++ backend/Server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+require("dotenv").config;
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("Api is running");
+});
+
+app.listen(5000,()=>{
+console.log("Listening port 5000");
+});
